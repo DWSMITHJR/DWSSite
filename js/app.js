64fdee94c16f4f9401f3edb1c90ab6473a2297a2
DWSMITHJR <donaldsmith@edgetek.com>
2025-11-11 00:48:27 +0000

    Deploy: 95807d9f2cf6cd95330684c1b1abb8417e443197

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 import { createParticles } from './animations/particles.js';
 import { observeElements, smoothScroll, headerScrollEffect } from './ui/scroll.js';
 import { initEmailForm } from './forms/email-form.js';
+import { initPanels } from './ui/panels.js';
 
 // Initialize all components when DOM is loaded
 document.addEventListener('DOMContentLoaded', () => {
@@ -10,4 +11,5 @@ document.addEventListener('DOMContentLoaded', () => {
     smoothScroll();
     headerScrollEffect();
     initEmailForm();
+    initPanels();
 });
